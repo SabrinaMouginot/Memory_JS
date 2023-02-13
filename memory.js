e7df1ec97249f57ecc5b5df7cf6946c1048b7d08
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-13 20:12:30 +0100

    style pour emplacement des cartes du score et des boutons

--- memory.js
+++ memory.js
@@ -42,3 +42,22 @@ fetch("./data/cards.json")
       cardElement.addEventListener("click", flipCard);
     }
   }
+
+  function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+  
+    this.classList.add("flipped");
+  
+    if (!firstCard) {
+      firstCard = this;
+      return;
+    }
+  
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+  
+    checkForMatch();
+  }
