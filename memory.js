85cdcf48030002a508dc144948c28d9776671756
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-13 17:35:28 +0100

    creation des 2 fonction shuffleCards et generateCards

--- memory.js
+++ memory.js
@@ -13,3 +13,32 @@ fetch("./data/cards.json")
     shuffleCards();
     generateCards();
   });
+
+  function shuffleCards() {
+    let currentIndex = cards.length,
+      randomIndex,
+      temporaryValue;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = cards[currentIndex];
+      cards[currentIndex] = cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
+    }
+  }
+  
+  function generateCards() {
+    for (let card of cards) {
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+      cardElement.setAttribute("data-name", card.name);
+      cardElement.innerHTML = `
+        <div class="front">
+          <img class="front-image" src=${card.image} />
+        </div>
+        <div class="back"></div>
+      `;
+      gridContainer.appendChild(cardElement);
+      cardElement.addEventListener("click", flipCard);
+    }
+  }
