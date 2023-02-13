c874beb01c7f40841c8412c31eaaba9229792dc9
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-13 23:09:01 +0100

    ajout de la partie dynamique du jeu ( retourner les cartes et compter le score )

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch("./data/cards.json")
+fetch("/data/cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
@@ -61,3 +61,40 @@ fetch("./data/cards.json")
   
     checkForMatch();
   }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  
+    isMatch ? disableCards() : unflipCards();
+  }
+  
+  
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+}
+
+function restart() {
+  resetBoard();
+  shuffleCards();
+  score = 0;
+  document.querySelector(".score").textContent = score;
+  gridContainer.innerHTML = "";
+  generateCards();
+}
